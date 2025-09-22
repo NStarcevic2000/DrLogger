65ae64a83dc130c4f20ba9b1cc692f69a83a86e7
NStarcevic2000 <nikola.starcevic2000@gmail.com>
2025-09-22 12:44:06 +0200

    Fix/find all doesnt work (#59)
    + Fix itterative find next
    + Fix "Find All" command
    + Remove option for non-collapsed data

--- gui/find_toolbar.py
+++ gui/find_toolbar.py
@@ -31,10 +31,10 @@ class FindToolbar(QToolBar):
         self.find_all_button.clicked.connect(self.find_all)
         self.addWidget(self.find_all_button)
 
-        self.find_in_collapsed_checkbox = QCheckBox("In collapsed rows")
-        self.find_in_collapsed_checkbox.setChecked(False)
-        self.find_in_collapsed_checkbox.setToolTip("Search in collapsed rows instead of what is rendered...")
-        self.addWidget(self.find_in_collapsed_checkbox)
+        # self.find_in_collapsed_checkbox = QCheckBox("In collapsed rows")
+        # self.find_in_collapsed_checkbox.setChecked(False)
+        # self.find_in_collapsed_checkbox.setToolTip("Search in collapsed rows instead of what is rendered...")
+        # self.addWidget(self.find_in_collapsed_checkbox)
 
         self.close_button = QToolButton()
         self.close_button.setText("✕")
@@ -61,7 +61,7 @@ class FindToolbar(QToolBar):
         self.hide()
 
     def restart_search(self):
-        idx_list = self.searchable_table.get_search_indexes(self.find_widget.text(), in_collapsed_data=True)
+        idx_list = self.searchable_table.get_search_indexes(self.find_widget.text(), show_collapsed=True)
         self.search_cache = (self.find_widget.text(), idx_list)
         self.searching_index = -1
         self.find_next()
@@ -87,16 +87,20 @@ class FindToolbar(QToolBar):
         self.find_next(_recursion_guard + 1)
     
     def find_all(self):
+        #TODO: Reimplement "show_collapsed" instead of "True"
         if self.find_widget.text() and self.find_widget.text() != self.search_all_cache[0] or \
-           self.find_in_collapsed_checkbox.isChecked() != self.search_all_cache[1]:
-            in_collapsed_data = self.find_in_collapsed_checkbox.isChecked()
+           True != self.search_all_cache[1]:
+            show_collapsed = True #self.find_in_collapsed_checkbox.isChecked()
             # If we already cached search indexes for "Find Next", reuse them
             # Since "Find Next" always searches in collapsed data, only reuse if searching in collapsed data
-            if in_collapsed_data and self.find_widget.text() == self.search_cache[0]:
+            if True and self.find_widget.text() == self.search_cache[0]:
                 idx_list = self.search_cache[1]
             else:
-                idx_list = self.searchable_table.get_search_indexes(self.find_widget.text(), in_collapsed_data=in_collapsed_data)
-            self.search_all_cache = (self.find_widget.text(), in_collapsed_data, idx_list)
+                # Iloc indexes
+                idx_list = self.searchable_table.get_search_indexes(self.find_widget.text(), show_collapsed=show_collapsed)
+                # Actual indexes
+                idx_list = [self.searchable_table.model()._visible_data.index[i] for i in idx_list]
+            self.search_all_cache = (self.find_widget.text(), show_collapsed, idx_list)
         # Show results in the footer table
         render_logs_table = FooterNotebook().get_widget(FOOTER_PAGE.FIND_RESULTS)
         if isinstance(render_logs_table, RenderedLogsTable):
